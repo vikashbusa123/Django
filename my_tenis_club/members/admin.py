4b3094f5b4aa2e6d7ecd359e4ae7bf3ad7099ca3
vikash123 <vikashbusa@gmail.com>
2023-12-28 16:23:51 +0530

    first commit

---
+++ my_tenis_club/members/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Member
+# Register your models here.
+class Memberadmin(admin.ModelAdmin):
+    list_display=("firstname","lastname","joined_date")
+admin.site.register(Member,Memberadmin)
